b307582a7e1758dc7188cf6641dace0f9349897d
Hasibur Rahman <hasib.active@gmail.com>
2021-12-10 21:21:24 +0600

    user validate

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -97,8 +97,22 @@ const validate = function (method) {
          return [
             body('firstName').notEmpty(),
             body('lastName').notEmpty(),
-            body('email').notEmpty(),
-            body('password').notEmpty()
+            body('email').notEmpty().custom(async (email) => {
+               const user = await User.findOne({ where: { email } })
+               if (user) {
+                  throw new Error('E-mail already in use')
+               }
+               return true
+            }),
+
+            body('password').notEmpty().isLength({ min: 6, max: 30 }),
+            body('confirmed_password').custom((value, { req }) => {
+               if (value !== req.body.password) {
+                  throw new Error('Password confirmation does not match password')
+               }
+               // Indicates the success of this synchronous custom validator
+               return true
+            })
          ]
          break
 
--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -4,10 +4,12 @@ const express = require("express")
 const router = express.Router({ mergeParams: true })
 
 /**
- * Alies to /categories
+ * Alies to /users
  */
 router.get('/', usersController.readAll)
 router.post('/', usersController.validate('create'), usersController.create)
+
+router.post('/register', usersController.validate('create'), usersController.create)
 
 router.get('/:id', usersController.readOne)
 router.put('/:id', usersController.validate('update'), usersController.update)
