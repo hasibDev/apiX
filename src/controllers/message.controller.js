78b1c1667fb119fb77f86ebc6224c5b065c27ee6
hasibdev <79460308+hasibdev@users.noreply.github.com>
2021-12-16 14:52:19 +0600

    message works

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,8 +1,54 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+   host: "smtp.mailtrap.io",
+   port: 2525,
+   secure: false, // upgrade later with STARTTLS
+   auth: {
+      user: "374270288d3980",
+      pass: "7e3cd3e0b155d9",
+   },
+})
+
 const sendMessage = (req, res) => {
+   const { name, email, message } = req.body
+   const content = `
+      <h2>Name: ${name}</h2>
+      <p>Email: ${email}</p>
+      <p>Message: ${message}</p>
+   `
 
+   // verify connection configuration
+   // transporter.verify(function (error, success) {
+   //    if (error) {
+   //       console.log(error)
+   //    } else {
+   //       console.log("Server is ready to take our messages")
+   //    }
+   // })
 
+   const sendMail = async () => {
+      try {
+         // send mail with defined transport object
+         let info = await transporter.sendMail({
+            from: '"ApiX Admin 👻" <admin@apix.com>', // sender address
+            to: email, // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: content, // html body
+         })
 
-   return res.json({ message: "Message Send." })
+         console.log("Message sent: %s", info.messageId)
+
+         return res.json({ message: "Message Send." })
+
+      } catch (error) {
+         return res.status(500).json({ error })
+      }
+   }
+
+   sendMail()
+
 }
 
 module.exports = {
