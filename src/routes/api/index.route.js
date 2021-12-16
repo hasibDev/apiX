0bf21bb6a36ac9d8e56e3875181dd54d79f19c1f
hasibdev <79460308+hasibdev@users.noreply.github.com>
2021-12-16 16:36:19 +0600

    removed message test

--- src/routes/api/index.route.js
+++ src/routes/api/index.route.js
@@ -1,12 +1,11 @@
 const categories = require('./categories.route')
 const users = require('./user.route')
-const message = require('./message.route')
 const express = require('express')
 
 const router = express.Router()
 
 router.use('/categories', categories)
 router.use('/users', users)
-router.use('/message', message)
+
 
 module.exports = router
